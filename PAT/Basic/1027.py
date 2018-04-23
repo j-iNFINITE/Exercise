ffd47632c01437657e596d7100c9dfc4ebb37fe2
Elivt <elivthrar@gmail.com>
2018-04-24 00:52:17 +0800

    update

--- PAT/Basic/1027.py
+++ PAT/Basic/1027.py
@@ -6,11 +6,20 @@ for i in range(1,500,2):
     if 2*i+1>=n:
         z=2*(i-1)+1
         y=n-z
-        countline=i-2-2
         break
-for e in range(countline,0,-2):
-    print(' '*int(((countline-e)/2))+s*e+' '*int(((countline-e)/2)))
-for e in range(1,countline+1,2):
-    if e==1:continue
-    print(' ' * int(((countline - e) / 2)) + s * e + ' ' * int(((countline - e) / 2)))
-print(y)
+print(y,z)
+z1=(z+1)/2
+parti=[]
+i=1
+while z1!=0:
+    parti.append(i)
+    z1-=i
+    i+=2
+linemax=parti[-1]
+for e in sorted(parti,reverse=True):
+    print(' '*int((linemax-e)/2)+e*s+' '*int((linemax-e)/2))
+for e in parti:
+    if parti.index(e)==0:continue
+    print(' ' * int((linemax - e) / 2) + e * s + ' ' * int((linemax - e) / 2))
+if y!=0:
+    print(y)
